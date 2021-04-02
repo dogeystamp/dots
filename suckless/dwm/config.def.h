15639af246b3a25ab88b08550ae475bd69448a7d
dogeystamp <dogeystamp@disroot.org>
2021-04-02 17:44:03 -0400

    [dwm] Add suspend bind
    (can be used to lock with xss-lock)

--- suckless/dwm/config.def.h
+++ suckless/dwm/config.def.h
@@ -67,11 +67,13 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, "-n", NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *suspendcmd[]  = { "systemctl", "suspend", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY|ShiftMask,             XK_s,      spawn,          {.v = suspendcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
