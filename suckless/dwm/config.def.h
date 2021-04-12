31ae26518b97e696076d57adbcf3ab6dd2473472
dogeystamp <dogeystamp@disroot.org>
2021-04-12 19:05:47 -0400

    [dwm] Actually fix swallow
    For some reason, last fix worked temporarily then broke

--- suckless/dwm/config.def.h
+++ suckless/dwm/config.def.h
@@ -46,7 +46,7 @@ static const Rule rules[] = {
 	 */
 	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
 	{ "Firefox", NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
-	{ "st",      NULL,     NULL,           0,         0,          1,           1,        -1 },
+	{ NULL,      NULL,     "st",           0,         0,          1,           1,        -1 },
 	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 };
 
