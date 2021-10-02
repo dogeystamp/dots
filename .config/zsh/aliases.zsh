28e5efd00e3e0381d69cac6c5777ea351b77b508
DogeyStamp <dogeystamp@disroot.org>
2021-10-02 11:29:37 -0400

    Update aliases
    Change spacing and add extra aliases

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -1,13 +1,24 @@
 # Color ls 
 alias ls='ls --color=auto'
+
 # Limit mpv to 1080p so it doesn't buffer
 alias mpv='mpv --ytdl-format="bestvideo[ext=mp4][height<=?1080]+bestaudio[ext=m4a]"'
+
 # Set gpg tty so curses pinentry works
 alias gpgt='export GPG_TTY=$(tty)'
+
 # Remove newlines from clipboard (for competitive programming)
 alias dnl='xsel -b | tr "\n" " " | xsel -ib'
+
 # Private neovim
 alias nvimp='nvim -u NONE -c "setlocal history=0 nobackup nomodeline noshelltemp noswapfile noundofile nowritebackup secure viminfo=\"\""'
+
+# Read pdf file as text
+alias pdfr='pdftotext $1 - | nvim'
+
 # Specific to my Gentoo system
 alias genlop='doas -u portage /usr/bin/genlop'
 alias loginctl='doas /bin/loginctl'
+
+# Delete my gpg key
+alias wipekey='gpg --batch --yes --delete-secret-keys 48B4FA19F1BC1603C85762514C53B0126F579F36'
