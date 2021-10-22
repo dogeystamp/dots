a87fa17acb676f8636931432890b26b979d56361
DogeyStamp <dogeystamp@disroot.org>
2021-10-21 20:56:16 -0400

    Don't use relative path in config.fish

--- src/.config/fish/config.fish
+++ src/.config/fish/config.fish
@@ -2,7 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-source .config/fish/aliases.fish
+source ~/.config/fish/aliases.fish
 
 set -gx PATH "$PATH:$HOME/.local/bin:"
 set fish_greeting ""
