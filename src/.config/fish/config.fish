66617ea73d2ee9b7389dcc8dcc0a048aeff37521
DogeyStamp <dogeystamp@disroot.org>
2021-10-31 15:09:26 -0400

    Add comments and gpgt to config.fish

--- src/.config/fish/config.fish
+++ src/.config/fish/config.fish
@@ -4,7 +4,14 @@ end
 
 source ~/.config/fish/aliases.fish
 
+# Set GPG_TTY
+gpgt
+
+# Add .local/bin to path
 set -gx PATH "$PATH:$HOME/.local/bin:"
+
+# Disable fish greeting
 set fish_greeting ""
 
+# Enable Vi bindings
 fish_vi_key_bindings
