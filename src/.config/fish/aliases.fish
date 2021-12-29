934d2542f4c4b8a7d2c603fada7d4aaebf116cbf
DogeyStamp <dogeystamp@disroot.org>
2021-12-29 11:36:22 -0500

    Miscellaneous fish alias changes

--- src/.config/fish/aliases.fish
+++ src/.config/fish/aliases.fish
@@ -2,9 +2,15 @@
 alias ls='ls --color=auto'
 
 # Limit mpv and youtube-dl to 1080p so it doesn't use too much bandwidth
-alias mpv='mpv --ytdl-format="bestvideo[ext=mp4][height<=?1080]+bestaudio[ext=m4a]"'
+#alias mpv='mpv --ytdl-format="bestvideo[ext=mp4][height<=?1080]+bestaudio[ext=m4a]"'
+
+# Run mpv from clipboard, replacing Invidious instances with YouTube's domain
+alias mpvy='mpv (xsel -b | sed "s/vid\.mint\.lgbt/youtube\.com/g" | sed "s/yewtu\.be/youtube\.com/g")'
 
 alias youtube-dl='youtube-dl -f "bestvideo[ext=mp4][height<=?1080]+bestaudio[ext=m4a]"'
+
+# Send screenshots to my VM
+alias scr='scp ~/med/screen/latest.png boron:~'
 
 # Set gpg tty so curses pinentry works
 alias gpgt='export GPG_TTY=(tty)'
@@ -23,6 +29,3 @@ end
 # Specific to my Gentoo system
 alias genlop='doas -u portage /usr/bin/genlop'
 alias loginctl='doas /bin/loginctl'
-
-# Delete my gpg key
-alias wipekey='gpg --batch --yes --delete-secret-keys A3A5FA72F8E5E54FBEE425057225FE3592EFFA38'
