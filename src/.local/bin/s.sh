9a7382f57d169fe42cf665a4846a3febc067320e
DogeyStamp <dogeystamp@disroot.org>
2021-12-29 11:38:37 -0500

    Change scrot syntax in s.sh and ss.sh
    Previous syntax does not work because of selection mode.

--- src/.local/bin/s.sh
+++ src/.local/bin/s.sh
@@ -2,4 +2,4 @@
 
 # Screenshot to clipboard, and leave a copy at ~/med/screen/latest.png
 
-sleep 0.1 && scrot -sf '/tmp/%F_%T_$wx$h.png' -e 'cp $f ~/med/screen/latest.png && xclip -selection clipboard -target image/png -i $f'
+sleep 0.1 && scrot -fs '/tmp/%F_%T_$wx$h.png' -e 'cp $f ~/med/screen/latest.png && xclip -selection clipboard -target image/png -i $f'
--- src/.local/bin/ss.sh
+++ src/.local/bin/ss.sh
@@ -2,4 +2,4 @@
 
 # Screenshot and save to disk.
 
-sleep 0.1 && scrot -sf '/tmp/%F_%T_$wx$h.png' -e 'cp $f ~/med/screen/latest.png && mv $f ~/med/screen/'
+sleep 0.1 && scrot -fs '/tmp/%F_%T_$wx$h.png' -e 'cp $f ~/med/screen/latest.png && mv $f ~/med/screen/'
