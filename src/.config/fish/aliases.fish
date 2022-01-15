577c690b9d314b95ac0d8c29f7d6f7332cf91c53
DogeyStamp <dogeystamp@disroot.org>
2022-01-15 16:25:26 -0500

    Reinstate mpv resolution restrictions

--- src/.config/fish/aliases.fish
+++ src/.config/fish/aliases.fish
@@ -2,7 +2,7 @@
 alias ls='ls --color=auto'
 
 # Limit mpv and youtube-dl to 1080p so it doesn't use too much bandwidth
-#alias mpv='mpv --ytdl-format="bestvideo[ext=mp4][height<=?1080]+bestaudio[ext=m4a]"'
+alias mpv='mpv --ytdl-format="bestvideo[ext=mp4][height<=?1080]+bestaudio[ext=m4a]"'
 
 # Run mpv from clipboard, replacing Invidious instances with YouTube's domain
 alias mpvy='mpv (xsel -b | sed "s/vid\.mint\.lgbt/youtube\.com/g" | sed "s/yewtu\.be/youtube\.com/g")'
