fc76ddd914449de2cb9dbc3ddc2d04df1b98e660
dogeystamp <dogeystamp@disroot.org>
2022-07-11 17:44:22 -0400

    msync, dotinstall.sh: rewrite scripts with options

--- dotinstall.sh
+++ dotinstall.sh
@@ -1,18 +1,94 @@
 #!/bin/sh
 
-cd $(dirname $(echo $0))/src/
+# Symlink dotfiles into the home directory
 
-echo Making directories
-for f in $(find -type d); do
-	DIR=$HOME$(echo $f | cut -c 2-)
-	echo $DIR
-	mkdir $DIR
+set -e
+
+SCRIPT_NAME="$(basename $0)"
+
+# Default source for dotfiles
+SRCFOLDER="$(dirname "$0")/src/"
+DESTFOLDER="$HOME"
+
+# Allow overwriting of outdated files
+FORCE="N"
+VERBOSE="N"
+
+display_help () {
+cat >&2 <<EOF 
+usage: $SCRIPT_NAME [-h] [-v] [-f] [-s PATH] [-d PATH]
+	-h: display help
+	-f: allow overwriting existing dotfiles
+	-v: print modified files
+	-s: path to source of dotfiles
+	-d: path to destination where dotfiles are linked from
+EOF
+}
+
+while getopts "fvhs:d:" o; do
+	case "$o" in
+		h) 
+			display_help
+			exit 0
+			;;
+		f) FORCE="Y" ;;
+		v) VERBOSE="Y" ;;
+		s)
+			DESTFOLDER="$OPTARG"
+			;;
+		d)
+			DESTFOLDER="$OPTARG"
+			;;
+	esac
 done
 
-echo Symlinking dotfiles
+if ! [ -d "$SRCFOLDER" ]; then
+	printf "$SCRIPT_NAME: source folder $SRCFOLDER is invalid.\n"
+	exit 1
+fi
+
+if ! [ -d "$DESTFOLDER" ]; then
+	printf "$SCRIPT_NAME: destination $DESTFOLDER is invalid.\n"
+	exit 1
+fi
+
+cd $SRCFOLDER
+
+for f in $(find -type d); do
+	DIR=$DESTFOLDER$(echo $f | cut -c 2-)
+	
+	if ! [ -d "$DIR" ]; then
+		if [ "$VERBOSE" = "Y" ]; then
+			printf "$DIR\n"
+		fi
+		mkdir $DIR
+	fi
+done
+
+link () {
+	SRC="$1"
+	DEST="$2"
+	OPTS="$3"
+
+	ln -s $OPTS "$SRC" "$DEST"
+
+	if [ "$VERBOSE" = "Y" ]; then
+		printf "$2\n"
+	fi
+}
+
 for f in $(find -type f); do
-	DEST=$HOME$(echo $f | cut -c 2-)
-	SRC=$PWD$(echo $f | cut -c 2-)
-	echo $SRC $DEST
-	ln -s $SRC $DEST
+	DEST="$DESTFOLDER$(echo $f | cut -c 2-)"
+	SRC="$PWD$(echo $f | cut -c 2-)"
+	if [ -e "$DEST" ]; then
+		if ! [ "$SRC" -ef "$DEST" ]; then
+			if [ "$FORCE" = "Y" ]; then
+				link "$SRC" "$DEST" -f
+			else
+				printf "$SCRIPT_NAME: $DEST already exists\n" >&2
+			fi
+		fi
+	else
+		link "$SRC" "$DEST"
+	fi
 done
