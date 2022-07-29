1b1f22ea38f1f995dd9086dfcc212d983619f54d
dogeystamp <dogeystamp@disroot.org>
2022-07-28 21:27:20 -0400

    [dwm] Increase gaps

--- suckless/dwm/config.def.h
+++ suckless/dwm/config.def.h
@@ -6,10 +6,10 @@
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int swallowfloating    = 1;        /* 1 means swallow floating windows by default */
-static const unsigned int gappih    = 35;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 40;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 30;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 40;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappih    = 70;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 80;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 90;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 110;       /* vert outer gap between windows and screen edge */
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static       int passthrough        = 0;        /* 1 means to ignore most shortcuts */
 static const int showbar            = 1;        /* 0 means no bar */
