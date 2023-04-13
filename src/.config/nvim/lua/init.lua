8d68ccd025358fd37bce89071fb5e6471446b737
dogeystamp <dogeystamp@disroot.org>
2023-04-13 11:36:02 -0400

    nvim: change autocomp bind to ctrl-f
    in line with fish

--- src/.config/nvim/lua/init.lua
+++ src/.config/nvim/lua/init.lua
@@ -94,8 +94,8 @@ require('leap').opts.safe_labels = {
 	"a", "s", "d", "f", "g", "h", "j", "k", "l"
 }
 require('leap').opts.labels = { "a", "s", "d",
-	"f", "k", "l", "h", "o", "d", "w", "e", "m", "b",
-	"u", "y", "v", "r", "g", "t", "c", "x", "/", "z",
+	"f", "k", "l", "h", "o", "d", "w", "e", "m",
+	"u", "v", "r", "g", "c", "x", "z",
 }
 
 local cmp = require'cmp'
@@ -105,11 +105,8 @@ cmp.setup({
 		documentation = cmp.config.window.bordered(),
 	},
 	mapping = cmp.mapping.preset.insert({
-	  ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-	  ['<C-f>'] = cmp.mapping.scroll_docs(4),
-	  ['<C-Space>'] = cmp.mapping.complete(),
 	  ['<C-e>'] = cmp.mapping.abort(),
-	  ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+	  ['<C-f>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
 	}),
 	sources = cmp.config.sources({
 		{ name = 'nvim_lsp' },
