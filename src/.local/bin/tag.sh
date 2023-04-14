2cc7ec8450f96e4a38bffacaf4fb48ff895375c3
dogeystamp <dogeystamp@disroot.org>
2023-04-13 22:05:09 -0400

    tag.sh: add track option

--- src/.local/bin/tag.sh
+++ src/.local/bin/tag.sh
@@ -5,9 +5,14 @@ cat "$TMPFILE" | while read line; do
 	link="$(echo $line | awk -F";" '{print $1}')"
 	artist="$(echo $line | awk -F";" '{print $2}')"
 	alb="$(echo $line | awk -F";" '{print $3}')"
-	file="$artist-$alb"
+	trk="$(echo $line | awk -F";" '{print $4}')"
+	file="$artist-$alb-$trk"
 	yt-dlp "$link" -x --audio-format mp3 --no-playlist -o "$file.%(ext)s"
 	mid3v2 -a "$artist" "$file"*
 	mid3v2 -A "$alb" "$file"*
-	mid3v2 -t "$alb" "$file"*
+	if [ -z "$trk" ]; then
+		mid3v2 -t "$alb" "$file"*
+	else
+		mid3v2 -t "$trk" "$file"*
+	fi
 done
