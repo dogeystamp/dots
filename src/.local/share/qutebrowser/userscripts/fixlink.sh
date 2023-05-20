e6747a4bd2c1fd883225a35697db9c95cadd7468
dogeystamp <dogeystamp@disroot.org>
2023-05-20 16:34:55 -0400

    qutebrowser: fixlink.sh - added Fandom wiki alternative

--- src/.local/share/qutebrowser/userscripts/fixlink.sh
+++ src/.local/share/qutebrowser/userscripts/fixlink.sh
@@ -11,12 +11,15 @@ REDDIT="lr.mint.lgbt"
 TWITTER="nitter.net"
 # genius lyrics
 GENIUS="sing.whatever.social"
+# fandom the wiki
+FANDOM="breezewiki.com"
 YOUTUBE="yewtu.be"
 
 LINK="$(printf "%s" "$QUTE_URL" | sed \
 	-e "s/www.reddit.com/$REDDIT/g" \
 	-e "s/twitter.com/$TWITTER/g" \
 	-e "s/genius.com/$GENIUS/g" \
+	-e "s/fandom.com/$FANDOM/g" \
 	-e "s/youtube.com/$YOUTUBE/g")"
 
 echo "open $1 $LINK" >> "$QUTE_FIFO"
