083aa183eb846105cad65b1ed2aa76b8c5e789d2
dogeystamp <dogeystamp@disroot.org>
2023-06-12 14:54:54 -0400

    clipedit.sh: use -c in st invocation

--- src/.local/bin/deskutils/clipedit.sh
+++ src/.local/bin/deskutils/clipedit.sh
@@ -4,7 +4,7 @@
 TMPFILE="$(mktemp)"
 
 #xsel -b > "$TMPFILE"
-st -g 60x8+0+800 -n popup-bottom-center \
+st -g 60x8+0+800 -c popup-bottom-center \
 	-e nvim -c "set binary noeol" -c "startinsert" "$TMPFILE" -c "highlight Normal ctermbg=016"
 cat "$TMPFILE" | xsel -ib
 rm "$TMPFILE"
