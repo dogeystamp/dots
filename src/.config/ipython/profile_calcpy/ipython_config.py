f2018687278f84be1a2f3a2f64b6e47c1079cbf8
dogeystamp <dogeystamp@disroot.org>
2023-06-15 11:29:02 -0400

    calcpy: improvements
    - escape actually works in vi-mode
    - added dg() alias for degrees()

--- src/.config/ipython/profile_calcpy/ipython_config.py
+++ src/.config/ipython/profile_calcpy/ipython_config.py
@@ -2,3 +2,4 @@ c = get_config()
 
 c.TerminalInteractiveShell.editing_mode = "vi"
 c.TerminalInteractiveShell.show_rewritten_input = False
+c.TerminalInteractiveShell.emacs_bindings_in_vi_insert_mode = False
