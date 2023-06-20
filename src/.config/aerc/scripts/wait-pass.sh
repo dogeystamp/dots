2fbb63af757e6ba6dff79f833b3b5fc0f63c7e3b
dogeystamp <dogeystamp@disroot.org>
2023-06-20 12:36:27 -0400

    aerc: wait-pass now tells you it's waiting

--- src/.config/aerc/scripts/wait-pass.sh
+++ src/.config/aerc/scripts/wait-pass.sh
@@ -1,5 +1,7 @@
 # https://man.sr.ht/~rjarry/aerc/integrations/password-manager.md
 
+echo "Waiting for you to open the password manager..." > /dev/tty
+
 secret-tool lookup "$1" "$2"
 # wait until the password is available
 while [ $? != 0 ]; do
