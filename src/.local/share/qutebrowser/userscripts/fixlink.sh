5371170dcf50b44addfe946b54e83e08541aa957
dogeystamp <dogeystamp@disroot.org>
2023-07-06 19:17:17 -0400

    qutebrowser: added libmedium to fixlink.sh

--- src/.local/share/qutebrowser/userscripts/fixlink.sh
+++ src/.local/share/qutebrowser/userscripts/fixlink.sh
@@ -14,12 +14,14 @@ GENIUS="sing.whatever.social"
 # fandom the wiki
 FANDOM="breezewiki.com"
 YOUTUBE="yewtu.be"
+MEDIUM="libmedium.batsense.net"
 
 LINK="$(printf "%s" "$QUTE_URL" | sed \
 	-e "s/www.reddit.com/$REDDIT/g" \
 	-e "s/twitter.com/$TWITTER/g" \
 	-e "s/genius.com/$GENIUS/g" \
 	-e "s/fandom.com/$FANDOM/g" \
+	-e "s/medium.com/$MEDIUM/g" \
 	-e "s/youtube.com/$YOUTUBE/g")"
 
 echo "open $1 $LINK" >> "$QUTE_FIFO"
