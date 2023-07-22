ec7dd6c5b9b244db919d0c68dbaa695a8f36fdd1
dogeystamp <dogeystamp@disroot.org>
2023-07-22 15:23:52 -0400

    ipython: default profile settings

---
+++ src/.config/ipython/profile_default/ipython_config.py
@@ -0,0 +1,5 @@
+c = get_config()
+
+c.TerminalInteractiveShell.editing_mode = "vi"
+c.TerminalInteractiveShell.show_rewritten_input = False
+c.TerminalInteractiveShell.emacs_bindings_in_vi_insert_mode = False
