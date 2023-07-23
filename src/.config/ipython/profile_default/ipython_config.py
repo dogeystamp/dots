5df98b9e39c391650dd70c37457503ad78b7ca66
dogeystamp <dogeystamp@disroot.org>
2023-07-22 21:13:12 -0400

    ipython: disable exit confirm prompt

--- src/.config/ipython/profile_default/ipython_config.py
+++ src/.config/ipython/profile_default/ipython_config.py
@@ -3,3 +3,4 @@ c = get_config()
 c.TerminalInteractiveShell.editing_mode = "vi"
 c.TerminalInteractiveShell.show_rewritten_input = False
 c.TerminalInteractiveShell.emacs_bindings_in_vi_insert_mode = False
+c.TerminalInteractiveShell.confirm_exit = False
