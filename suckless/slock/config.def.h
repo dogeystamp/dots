7065fc03f6a415ff885f4a8e3e80ee0a8294b5ad
dogeystamp <dogeystamp@disroot.org>
2024-01-02 13:07:44 -0500

    [slock] change colors

--- suckless/slock/config.def.h
+++ suckless/slock/config.def.h
@@ -4,8 +4,8 @@ static const char *group = "nogroup";
 
 static const char *colorname[NUMCOLS] = {
 	[INIT] =   "black",     /* after initialization */
-	[INPUT] =  "#005577",   /* during input */
-	[FAILED] = "#CC3333",   /* wrong password */
+	[INPUT] =  "#111111",   /* during input */
+	[FAILED] = "#551111",   /* wrong password */
 };
 
 /* treat a cleared input like a wrong password (color) */
