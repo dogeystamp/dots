9bed33734138a6e1a4723e9a9300521c67fb772d
dogeystamp <dogeystamp@disroot.org>
2024-01-02 11:38:04 -0500

    [st] patch: newterm (orphan variant)

--- suckless/st/config.def.h
+++ suckless/st/config.def.h
@@ -206,6 +206,7 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
 	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
 	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
+	{ TERMMOD,              XK_Return,      newterm,        {.i =  0} },
 };
 
 /*
