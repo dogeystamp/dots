e8308206bf834577c6460b0be5eb448379c6d226
dogeystamp <dogeystamp@disroot.org>
2024-06-07 10:59:30 -0400

    picom: transparency for feishin

--- src/.local/bin/picomstart.sh
+++ src/.local/bin/picomstart.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 # misc opts so i don't have to use absolute path in picom config
 
-picom --window-shader-fg-rule "$HOME/.config/picom/shaders/transparency.glsl:class_g = 'qutebrowser'"
+picom  --window-shader-fg-rule "$HOME/.config/picom/shaders/transparency-sonixd.glsl:class_g = 'feishin'" \
+	--window-shader-fg-rule "$HOME/.config/picom/shaders/transparency.glsl:class_g = 'qutebrowser'" \
