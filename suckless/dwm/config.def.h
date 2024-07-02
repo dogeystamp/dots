857b121e36730da1f0ed68707a3fdabe11d9f849
dogeystamp <dogeystamp@disroot.org>
2024-07-02 14:18:22 +0800

    [dwm] closer binds for tags 5-6

--- suckless/dwm/config.def.h
+++ suckless/dwm/config.def.h
@@ -179,8 +179,13 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
 	TAGKEYS(                        XK_4,                      3)
+	// odd keybinds here to bring tags closer to my fingers
+	TAGKEYS(                        XK_e,                      4)
+	TAGKEYS(                        XK_r,                      5)
+	// backwards compatibility
 	TAGKEYS(                        XK_5,                      4)
 	TAGKEYS(                        XK_6,                      5)
+
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
