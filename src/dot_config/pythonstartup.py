bdcbb19b08196cf84912a8b290108571e9677607
dogeystamp@disroot.org <dogeystamp@disroot.org>
2024-12-10 22:32:11 -0500

    pythonstartup.py: improvements

--- src/dot_config/pythonstartup.py
+++ src/dot_config/pythonstartup.py
@@ -1,7 +1,48 @@
-try:
-    from sympy import init_session, Rational
-    init_session()
+"""
+bashrc but for Python
 
-    R = Rational
+Runs on both Python and IPython.
+"""
+
+from os import environ
+
+print(f"\nUsing PYTHONSTARTUP file: {environ.get("PYTHONSTARTUP")}")
+print("Read this file for information about custom macros/utilities.")
+
+ipy = None
+
+try:
+    from IPython.core.getipython import get_ipython
+    ipy = get_ipython()
 except ImportError:
     pass
+
+def math_mode():
+    from sympy import init_session, Rational, pi
+
+    init_session()
+
+    global R
+    R = Rational
+
+    # eval last line
+    if ipy:
+        ipy.define_macro("ev", "N(_)")
+
+    global radians
+    def radians(angle):
+        return angle * 2 * pi / 360
+
+def data_mode():
+    try:
+        import numpy as np
+    except ImportError:
+        pass
+    try:
+        import pandas as pd
+    except ImportError:
+        pass
+    try:
+        import matplotlib.pyplot as plt
+    except ImportError:
+        pass
