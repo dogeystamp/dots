d1aa4bfc805cb59f88e5b672e836d64bb81cab57
dogeystamp <dogeystamp@disroot.org>
2025-04-25 23:03:34 -0400

    setup to use neovide as a terminal
    truly an emacs moment

--- src/dot_local/bin/executable_newterm.sh
+++ src/dot_local/bin/executable_newterm.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 # wrapper to either start alacritty or just create a new window
 
-alacritty msg create-window "$@" || alacritty "$@"
+if [ -n "$EYECANDY" ]; then
+	neovide -- -c "set ls=0" -c "startinsert" -c "au TermClose * :qa" -c "set shell=$SHELL" -c "term $@"
+else
+	alacritty msg create-window "$@" || alacritty "$@"
+fi
