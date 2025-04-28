50e29f0fdd1c73ca42d35022045d81518c9cd946
dogeystamp@disroot.org <dogeystamp@disroot.org>
2025-04-27 20:19:08 -0400

    neovide: custom fast terminal init.vim

--- src/dot_local/bin/executable_newterm.sh
+++ src/dot_local/bin/executable_newterm.sh
@@ -2,7 +2,7 @@
 # wrapper to either start alacritty or just create a new window
 
 if [ -n "$EYECANDY" ]; then
-	neovide -- -c "set ls=0" -c "startinsert" -c "au TermClose * :qa" -c "set shell=$SHELL" -c "term $@"
+	neovide -- -u "$XDG_CONFIG_HOME/nvim/init_term.vim" -c "startinsert" -c "set shell=$SHELL" -c "term $@"
 else
 	alacritty msg create-window "$@" || alacritty "$@"
 fi
