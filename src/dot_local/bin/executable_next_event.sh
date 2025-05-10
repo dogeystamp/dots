466e817fc42663982eb3085e7004f6ef4a141dc0
dogeystamp@disroot.org <dogeystamp@disroot.org>
2025-05-09 22:26:21 -0400

    next_event.sh: display events once

--- src/dot_local/bin/executable_next_event.sh
+++ src/dot_local/bin/executable_next_event.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 if command -v khal > /dev/null; then
-	khal list  -df "" -f "{cancelled}{title} {location} ({start-style} {to-style} {end-style})" --notstarted now 9h | head -n 1
+	khal list  -df "" -f "{cancelled}{title} {location} ({start-style} {to-style} {end-style})" --once --notstarted now 9h | head -n 1
 fi
