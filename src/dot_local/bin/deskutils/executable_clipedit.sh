1ee558375520fa9dcb2171d6e075f0cdb4575f68
dogeystamp@disroot.org <dogeystamp@disroot.org>
2025-05-09 22:26:55 -0400

    clipedit.sh: explicitly use alacritty
    the options don't make sense with newterm.sh (which is for terminals in
    general)

--- src/dot_local/bin/deskutils/executable_clipedit.sh
+++ src/dot_local/bin/deskutils/executable_clipedit.sh
@@ -5,9 +5,8 @@ TMPFILE="$(mktemp)"
 
 cb > "$TMPFILE"
 
-newterm.sh \
+alacritty \
 	-o 'window.class = { instance = "popup-bottom-center", general="popup-bottom-center" }' \
-	-o 'window.opacity = 1.0' \
 	-o 'window.dimensions = { columns = 84, lines = 10 }' \
 	-o 'window.position = { x = 0, y = 1200 }' \
 	-e sh -c "cb > '$TMPFILE'; nvim -c 'set binary noeol textwidth=80' '$TMPFILE' && cat '$TMPFILE' | cb set && rm '$TMPFILE'"
