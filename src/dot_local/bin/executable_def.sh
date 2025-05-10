b5c9266d5af32a50035c6aa95d2704251bc7cc46
dogeystamp@disroot.org <dogeystamp@disroot.org>
2025-05-09 22:25:42 -0400

    def.sh: use elinks to render (with color)

--- src/dot_local/bin/executable_def.sh
+++ src/dot_local/bin/executable_def.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 # sdcv dictionary wrapper (for HTML rendering)
+# requires gnu sed for formatting tweaks
 
-sdcv -n --utf8-output "$@" 2>&1 | \
-	w3m -T text/html -dump -
+cyan=$(echo -ne "\e[0;36m")
+reset=$(echo -ne "\e[0m")
+
+sdcv -n --utf8-output "$@" 2>&1 \
+	| elinks -dump -dump-color-mode 1 -no-references -no-connect -localhost \
+	| sed -E 's/Found.*similar to [a-zA-Z]+\.//g' \
+	| sed -E "s/[ ]{2,}-->/\n$cyan\n-->/g" \
+	| sed "s/ -->/$reset\n\n/g"
