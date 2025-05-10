bdb94aea6f2ff41ca2aba02808eaf8fdda7bd27e
dogeystamp@disroot.org <dogeystamp@disroot.org>
2025-05-09 22:25:23 -0400

    newterm: use exec to reduce process count

--- src/dot_local/bin/executable_newterm.sh
+++ src/dot_local/bin/executable_newterm.sh
@@ -2,7 +2,7 @@
 # wrapper to either start alacritty or just create a new window
 
 if [ -n "$EYECANDY" ]; then
-	neovide -- -u "$XDG_CONFIG_HOME/nvim/init_term.vim" -c "startinsert" -c "set shell=$SHELL" -c "term $@"
+	exec neovide -- -u "$XDG_CONFIG_HOME/nvim/init_term.vim" -c "startinsert" -c "set shell=$SHELL" -c "term $@"
 else
-	alacritty msg create-window "$@" || alacritty "$@"
+	alacritty msg create-window "$@" || exec alacritty "$@"
 fi
