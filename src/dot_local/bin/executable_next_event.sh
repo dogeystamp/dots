250cb2ef83afc009683e1e1869f2e53b245ee79f
dogeystamp@disroot.org <dogeystamp@disroot.org>
2025-05-25 22:33:41 -0400

    waybar: list next next event too

--- src/dot_local/bin/executable_next_event.sh
+++ src/dot_local/bin/executable_next_event.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
 if command -v khal > /dev/null; then
-	khal list  -df "" -f "{cancelled}{title} {location} ({start-style} {to-style} {end-style})" --once --notstarted now 9h | head -n 1
+	# list events from now till end of day, except all day events
+	khal list \
+		-df "" \
+		-f "{cancelled}{title} {location} ({start-style} {to-style} {end-style})" \
+		now eod \
+		| grep -v "(→ - →)" \
+		| head -n 2
 fi
