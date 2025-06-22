5ae62a8cb9db3825722364ce7fb54943b7ebd2de
dogeystamp@disroot.org <dogeystamp@disroot.org>
2025-06-21 20:49:59 -0400

    dunst: remove random stuff

--- src/dot_local/bin/deskutils/executable_notification-sound.sh
+++ src/dot_local/bin/deskutils/executable_notification-sound.sh
@@ -1,12 +1,4 @@
 #!/bin/sh
 
-# Dunst notification sound script.
-
-if [ "$DUNST_APP_NAME" = "soundboard" ]; then
-	exit
-fi
-if [ "$DUNST_APP_NAME" = "prod" ]; then
-	exit
-fi
-
+# Notification sound script.
 paplay ~/.local/bin/deskutils/notif.wav
