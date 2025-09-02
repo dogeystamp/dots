8272e792ec8665c8b29f6f03162ffaa9dfca412d
dogeystamp <dogeystamp@disroot.org>
2025-09-01 22:21:08 -0400

    prod.sh: use timewarrior instead of watson
    the rationale is that timew is in arch repos, while watson isn't.

--- src/dot_local/bin/deskutils/executable_prod.sh
+++ src/dot_local/bin/deskutils/executable_prod.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-# visual reminder of watson status
+# visual reminder of time-tracking status
 
 while true; do
-	notify-send -a "prod" -r 13371234 "$(watson status)"
+	notify-send -a "prod" -r 13371234 "$(timew)"
 	sleep 5
 done
