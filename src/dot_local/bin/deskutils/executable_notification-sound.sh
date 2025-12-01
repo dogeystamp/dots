3bcee6feaf9d6a99018d262a4216b390650b7588
dogeystamp <dogeystamp@disroot.org>
2025-11-30 21:04:25 -0500

    dunst: remove exceptions for notif sounds
    instead, set low priority to avoid notif sound

--- src/dot_local/bin/deskutils/executable_notification-sound.sh
+++ src/dot_local/bin/deskutils/executable_notification-sound.sh
@@ -2,11 +2,4 @@
 
 # Dunst notification sound script.
 
-if [ "$DUNST_APP_NAME" = "soundboard" ]; then
-	exit
-fi
-if [ "$DUNST_APP_NAME" = "prod" ]; then
-	exit
-fi
-
 paplay ~/.local/bin/deskutils/notif.wav
