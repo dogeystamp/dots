76757d857ec87a910ad837c60ba4414a4dbad578
dogeystamp <dogeystamp@disroot.org>
2025-11-30 21:01:43 -0500

    ping-up: added ascii art directly (instead of requiring figlet)

--- src/dot_local/bin/executable_ping-up.sh
+++ src/dot_local/bin/executable_ping-up.sh
@@ -5,10 +5,17 @@ while true; do
 	if ping -c 1 $1; then
 		echo Ping successful
 		notify-send -a "ping-up" "Ping is successful"
-		figlet Ping successful
+		cat << 'EOF'
+   ___ _               _ 
+  / _ (_)_ __   __ _  / \
+ / /_)/ | '_ \ / _` |/  /
+/ ___/| | | | | (_| /\_/ 
+\/    |_|_| |_|\__, \/   
+               |___/     
+EOF
 		tput bel
 		break
 	else
-		sleep 5
+		sleep 1
 	fi
 done
