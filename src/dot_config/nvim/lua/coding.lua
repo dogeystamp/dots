912c9ea33e4270aec72b2e65f6436066c6800f60
dogeystamp <dogeystamp@disroot.org>
2026-04-21 16:44:01 -0400

    nvim: fix vim gitgutter packadd order

--- src/dot_config/nvim/lua/coding.lua
+++ src/dot_config/nvim/lua/coding.lua
@@ -191,10 +191,10 @@ end, {})
 -- git gutter / sign column
 ----------------------------
 
-vim.cmd.packadd("vim-gitgutter")
 vim.g.gitgutter_sign_added = '▐'
 vim.g.gitgutter_sign_modified = '▐'
 vim.g.gitgutter_sign_removed = '▐'
 vim.g.gitgutter_sign_removed_first_line = '▐'
 vim.g.gitgutter_sign_removed_above_and_below = '▐'
 vim.g.gitgutter_sign_modified_removed = '▐'
+vim.cmd.packadd("vim-gitgutter")
